8453a504b394d5e057b87879d96f6198e9992b43
Martin Marcos <mmarcos.arg@gmail.com>
2015-10-28 01:40:49 -0300

    Se modificaron el socket server y client para que envien el caracter null al final del string.

--- eclipse_c_projects/socket_server/src/socket_server.c
+++ eclipse_c_projects/socket_server/src/socket_server.c
@@ -56,7 +56,7 @@ int main() {
 	while( (read_size = recv(client_sock , client_message , 2000, 0)) > 0 )
 	{
 		//Send the message back to client
-		write(client_sock , client_message , strlen(client_message));
+		write(client_sock , client_message , strlen(client_message)+1);
 		puts(client_message);
 	}
 
